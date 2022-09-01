2e74be6adb434af5690e3ad478001e1d683b3e65
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2022-09-01 11:40:55 -0700

    dowload

--- WebCrawler/src/Main.java
+++ WebCrawler/src/Main.java
@@ -1,6 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.io.*;
 
-        System.out.println("Hello world!");
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://www.surlatable.com/recipes/?srule=best-matches&start=0&sz=24")).GET().build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+
+        //output to a file, so it's easy to mess with (you won’t be for your finished program)
+        FileOutputStream fs = new FileOutputStream("Site.txt");
+        PrintWriter pw = new PrintWriter(fs);
+        pw.println(response.body()); //response.body() is the html source code in a string format. It outputs to a file so you can see it easier right now, but you will ultimately want to just manipulate the strings a lot
+        pw.close();
     }
 }
