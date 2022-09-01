2fc6127566e3bb5f33844cbb3fad0ea942735358
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2022-09-01 15:22:26 -0700

    idk

--- WebCrawler/src/Main.java
+++ WebCrawler/src/Main.java
@@ -9,6 +9,8 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
+
+        //get request to grab site.main
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://www.surlatable.com/recipes/?srule=best-matches&start=0&sz=24")).GET().build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -17,7 +19,7 @@ public class Main {
         //output to a file, so it's easy to mess with (you won’t be for your finished program)
         FileOutputStream fs = new FileOutputStream("Site.txt");
         PrintWriter pw = new PrintWriter(fs);
-        pw.println(response.body()); //response.body() is the html source code in a string format. It outputs to a file so you can see it easier right now, but you will ultimately want to just manipulate the strings a lot
+        pw.println(response.body()); //response.body() is the html source code in a string format. It outputs to a file, so you can see it easier right now, but you will ultimately want to just manipulate the strings a lot
         pw.close();
     }
 }
