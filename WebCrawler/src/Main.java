903420d5e1256b3b3590ceebc4f45b803ed90729
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2022-09-01 15:45:16 -0700

    up

--- WebCrawler/src/Main.java
+++ WebCrawler/src/Main.java
@@ -15,11 +15,15 @@ public class Main {
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-
         //output to a file, so it's easy to mess with (you won’t be for your finished program)
         FileOutputStream fs = new FileOutputStream("Site.txt");
         PrintWriter pw = new PrintWriter(fs);
         pw.println(response.body()); //response.body() is the html source code in a string format. It outputs to a file, so you can see it easier right now, but you will ultimately want to just manipulate the strings a lot
         pw.close();
+
+        
+
+
+
     }
 }
