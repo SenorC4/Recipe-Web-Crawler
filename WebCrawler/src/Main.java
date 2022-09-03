6ce89d8974a1b3d25caba61f67c220287fccd306
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2022-09-03 04:15:11 -0700

    Need to fix start and end strings
    Also need to ask around about html decoding escape characters

--- WebCrawler/src/Main.java
+++ WebCrawler/src/Main.java
@@ -6,13 +6,11 @@ import java.net.http.HttpResponse;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
-import java.net.URLDecoder;
-
 
 public class Main {
 
     ///change this for different number of recipes
-    static int numOfRecipes = 4;
+    static int numOfRecipes = 96;
     //Change this for different number of recipes
 
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -31,8 +29,11 @@ public class Main {
         for(int i = 0; i < numOfRecipes; i++){
             //name,Author,Path,Name,Servings,Ingredients,Instructions
             String recipe = "";
-            int start = 0;
-            int end = 0;
+            int start;
+            int end;
+
+            //Delay for the sake of Sur La Tables infrastructure
+            TimeUnit.SECONDS.sleep(10);
 
             //get recipe site
             HttpClient client = HttpClient.newHttpClient();
@@ -40,65 +41,89 @@ public class Main {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
             //get name
-            String name = "";
+            System.out.println(i + " 1");
+            String name;
             start = response.body().indexOf("recipe-name") + 13;
             end = response.body().indexOf("</h1>", start);
             name = response.body().substring(start, end);
+
+            name = formatRecipe(name);
+
             recipe+= name + ",";
 
 
             //get author
+            System.out.println(i + " 2");
             start = response.body().indexOf("By ", end) + 3;
             end = response.body().indexOf("\n", start);
             String author = response.body().substring(start, end) + ",";
-            //author = URLDecoder.decode(author);
-            author = author.replaceAll("\\<.*?\\>" , "").replace("\n", " ").replace(",", "");
-            recipe += author + ",";
 
+            author = formatRecipe(author);
+            recipe += author + ",";
+            System.out.println(author);
 
             //get path
+            System.out.println(i + " 3");
             start = response.body().lastIndexOf("\"@id\":\"https://www.surlatable.com/recipes/", end) + 33;
-            end = response.body().indexOf("\",\"", start);
-            recipe += "home" + response.body().substring(start, end) + name + ",";
+            end = response.body().indexOf("name", start) - 3;
+            String path = "home" + response.body().substring(start, end) + name + ",";
+            System.out.println(path);
+            System.out.println(name);
+            recipe += path;
+
 
             //get servings
-            if(response.body().indexOf("Makes") > 1) {
-                start = response.body().indexOf("Makes") + 6;
+            System.out.println(i + " 4");
+            String servings;
 
-                if(response.body().indexOf("ervings", start) > 1)
-                    end = response.body().indexOf("ervings", start) - 2;
-                else if(response.body().indexOf(" cups", start) > 1){
-                    end = response.body().indexOf(" cups", start);
-                }
+            if(response.body().indexOf("recipe-details-serves") > 1) {
+                start = response.body().indexOf("recipe-details-serves") + 29;
+                end = response.body().indexOf("</div>\n", start)-1;
 
-                recipe += response.body().substring(start, end) + ",";
+                servings = response.body().substring(start, end);
+                servings = formatRecipe(servings);
+                recipe += servings + ",";
             }else{
-                recipe += "1,";
+                servings = "1,";
+                recipe += servings;
             }
 
+
+
+//            if(response.body().indexOf("ervings", start) > 1)
+//                end = response.body().indexOf("ervings", start) - 2;
+//            else if(response.body().indexOf(" cups", start) > 1){
+//                end = response.body().indexOf(" cups", start);
+//            }else{
+//                servings = "1";
+//            }
+
+
+
             //get ingredients
+            System.out.println(i + " 5");
             start = response.body().indexOf("recipe-details-ingredients") + 34;
-            end = response.body().indexOf("<br><br>", start);
+            end = response.body().indexOf("</div>\n", start);
             String ingredients = response.body().substring(start, end) + ",";
 
-            ingredients = ingredients.replaceAll("\\<.*?\\>" , "").replace("\n", " ").replace(",", "");
+            ingredients = formatRecipe(ingredients);
 
             recipe += ingredients + ",";
 
             //get Instructions
+            System.out.println(i + " 6");
             start = response.body().indexOf("recipe-details-procedure") + 27;
-            end = response.body().indexOf("<br><br>\n", start);
+            end = response.body().indexOf("</div>\n", start);
             String instructions = response.body().substring(start, end) + ",";
 
-            instructions = instructions.replaceAll("\\<.*?\\>" , "").replace("\n", " ").replace(",", "");
+            instructions = formatRecipe(instructions);
 
             recipe += instructions;
 
-            //fix html encode for stupid names (L&eacute'ku&eacute;)
-
-
+            //put one recipe line into main string that gets written
             out += recipe + "\n";
 
+            //debug only
             System.out.println(i + ": " + out);
         }
 
@@ -128,7 +153,7 @@ public class Main {
         //get request each site page and grab 24 recipe urls
         for(int i = 0; i < urls.size();i++ ) {
             System.out.println("Getting first file");
-            int start = 0;
+            int start;
             int end = 0;
             //get request to grab site.main
             HttpRequest request = HttpRequest.newBuilder().uri(URI.create(urls.get(i))).GET().build();
@@ -151,10 +176,19 @@ public class Main {
         return recipeUrls;
     }
 
-    public static String formatRecipe(String instructions){
+    public static String formatRecipe(String stuff){
+        stuff = stuff.replaceAll("\\<.*?\\>" , "").replace("\n", " ").replace(",", "");
+
+        //i hate this, this sucks, please give me better libraries for html escape characters. Oracle I love you, but no
+        stuff = stuff.replaceAll("&rsquo;", "'");
+        stuff = stuff.replaceAll("&#38;#176;", "°");
+        stuff = stuff.replaceAll("&#38;#38;", "&");
+        stuff = stuff.replaceAll("&#38;amp;", "&");
+        stuff = stuff.replaceAll("&amp;", "&");
+        stuff = stuff.replaceAll("&#38;", "&");
 
 
-        return instructions;
+        return stuff;
     }
 
 }
